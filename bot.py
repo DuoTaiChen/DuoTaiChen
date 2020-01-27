c1c98d7abfbb71a93b1897f857bb08652d0173da
DuoTaiChen <60336199+DuoTaiChen@users.noreply.github.com>
2020-01-27 14:48:20 +0800

    DuoTaiChen

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord 
+from discord.ext import commands
+
+bot=commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(">>Bot is online<<")
+
+@bot.event
+async def on_memver_join(member):
+    print(f'{member}光臨了比薩店')
+
+@bot.event
+async def on_memver_remove(member):
+        print(f'{member}離開了比薩店')
+
+bot.run('NjcxMjIzODQyNzk4MTc0MjA4.Xi5-pQ.avHGUO05axvxf1iC2moo5DNE8UA')
