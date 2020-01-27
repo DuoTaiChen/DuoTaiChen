cc8f7596ecf5200e38ea238de4b9563d7dc08436
DuoTaiChen <60336199+DuoTaiChen@users.noreply.github.com>
2020-01-27 14:56:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ async def on_ready():
     print(">>Bot is online<<")
 
 @bot.event
-async def on_memver_join(member):
+async def on_member_join(member):
     print(f'{member}光臨了比薩店')
 
 @bot.event
-async def on_memver_remove(member):
+async def on_member_remove(member):
     print(f'{member}離開了比薩店')
 
 bot.run('NjcxMjIzODQyNzk4MTc0MjA4.Xi5-pQ.avHGUO05axvxf1iC2moo5DNE8UA')
